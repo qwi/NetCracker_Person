d8b54ec5e0f04cc8c8c0bd7401c65083d58da411
Oleg <hspqwi@gmail.com>
2019-12-03 21:44:42 +0300

    Added test for Reflection

---
+++ src/test/java/com/netcracker/edu/trainee/firstLab/entities/repository/service/reflexion/TestReflection.java
@@ -0,0 +1,22 @@
+package com.netcracker.edu.trainee.firstLab.entities.repository.service.reflexion;
+
+import com.netcracker.edu.trainee.firstLab.entities.Person;
+import com.netcracker.edu.trainee.firstLab.service.reflexion.Reflector;
+import com.netcracker.edu.trainee.firstLab.service.repository.DynamicArray;
+import com.netcracker.edu.trainee.firstLab.service.sort.BubbleSort;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestReflection {
+    @Test
+    public void testCheckWhatReturnReflection() throws NoSuchFieldException, IllegalAccessException {
+        DynamicArray<Person> repository = new DynamicArray<>();
+        Reflector.reflect(repository);
+        Field field = repository.getClass().getDeclaredField("sort");
+        field.setAccessible(true);
+        assertEquals(field.get(repository).getClass(), BubbleSort.class);
+    }
+}
